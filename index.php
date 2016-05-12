db01ae624d3c0f4bec85b558ed5b004a4f39f643
phpyangyang <245983054@qq.com>
2016-05-12 16:47:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 //这是我的第一个github测试文件
 phpinfo();
+
+//将改动的地方添加到版本管理器
+//git add .
+
+//提交到本地的版本控制库里
+//git commmit -m "changes log"
+
+//将本地仓库提交到github的帐号里
+//git push -u origin master
+
+//同步到本地 git fetch
